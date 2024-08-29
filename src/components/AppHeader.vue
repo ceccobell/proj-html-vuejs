89071362870d1b51924d63b085ba8d31c6caebab
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-08-29 16:25:19 +0200

    [UPD] done basic structure of navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,37 @@
 <template>
-    
+    <header>
+        <div class="navbar d-flex justify-content-between">
+            <!-- Sezione sinistra -->
+            <div class="navbar-left">
+                <button>ORDER ONLINE</button>
+            </div>
+
+            <!-- Sezione centrale -->
+            <div class="navbar-center">
+                <ul class="list-unstyled d-flex">
+                    <li v-for="(item, index) in navbarCenterItems" :key="index">
+                        <a :href="item.href" v-if="item.type === 'link'">
+                            <img v-if="item.icon" :src="item.icon" class="icon" alt="icon" />
+                            {{ item.text }}
+                        </a>
+                        <img v-if="item.type === 'image'" :src="item.src" :alt="item.alt" />
+                    </li>
+                </ul>
+            </div>
+
+            <!-- Sezione destra -->
+            <div class="navbar-right">
+                <ul class="list-unstyled d-flex">
+                    <li v-for="item in navbarRightItems" :key="item.text">
+                        <a :href="item.href">
+                            <img v-if="item.icon" :src="item.icon" class="icon" alt="icon" />
+                            {{ item.text }}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -12,5 +44,7 @@ export default {
 </script>
 
 <style scoped>
-    
+    header {
+        background-image: url(../../public/img/cielostellato.PNG);
+    }
 </style>
