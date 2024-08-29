4cc7269efe42993e00558a747752dba8a39d3019
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-08-29 16:11:44 +0200

    [UPD] created array with link of navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader :navbarCenterItems="navbarCenter" :navbarRightItems="navbarRight" />
 </template>
 
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  components: {
+    AppHeader
+  },
+  data () {
+    return {
+      navbarCenter: [
+        { type: 'link', text: 'HOME', href: '#', icon: '/public/svg/svg-0.svg' },
+        { type: 'link', text: 'PAGES', href: '#' },
+        { type: 'link', text: 'MENU', href: '#' },
+        { type: 'image', src: '/public/img/logo.png', alt: 'Logo' },
+        { type: 'link', text: 'EVENT', href: '#' },
+        { type: 'link', text: 'BLOG', href: '#' },
+        { type: 'link', text: 'LANDING', href: '#' }
+      ],
+      navbarRight: [
+        { type: 'link', text: 'Cart', href: '#', icon: '/public/svg/svg-1.svg' },
+        { type: 'link', text: 'SEARCH', href: '#' }
+      ]
+    }
+  }
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  
 </style>
