7bf5530de5f2f44ff269e47bd9bbe698c57b0caf
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-08-30 17:24:01 +0200

    [UPD] created cycle for the pizza menu

--- src/components/MenuPizza.vue
+++ src/components/MenuPizza.vue
@@ -9,6 +9,15 @@
                     <img src="/public/svg/svg-4.svg" alt="" class="icon-pizza-img">
                 </div>
             </div>
+            <div class="col-100">
+                <ul class="list-unstyled d-flex">
+                    <li v-for="pizza,index in menuPizza" :key="index" class="text-center">
+                        <img :src="pizza.src" alt="">
+                        <div class="text-tertiary">{{ pizza.nome }}</div>
+                        <div class="text-danger">{{ pizza.costo_massimo }}</div>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -55,7 +64,7 @@ export default {
                 },
                 {
                     nome: 'CAMPAGNOLA',
-                    src: '/public/img/h3-product-img-6a-150x150.png',
+                    src: '/public/img/h3-product-img-6a-100x100.png',
                     sold: false,
                     costo_minimo: '$50.00',
                     costo_massimo: '$95.00'
@@ -69,5 +78,10 @@ export default {
 <style scoped>
     .col-100 {
         margin: 30px 0px;
+    }
+
+    li img {
+        width: 150px;
+        height: auto;
     }
 </style>
