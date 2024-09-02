236763325df6315b62e73aadb6a86f02bcb4a26d
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-09-02 11:44:28 +0200

    [UPD] completed section for menu pizza

--- src/components/MenuPizza.vue
+++ src/components/MenuPizza.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="container position-relative">
         <div class="row">
             <div class="col-100 text-center position-relative">
                 <div class="fs-10 text-danger my-10">CHOOSE YOUR FLAVOR</div>
@@ -10,13 +10,19 @@
                 </div>
             </div>
             <div class="col-100">
-                <ul class="list-unstyled d-flex">
-                    <li v-for="pizza,index in menuPizza" :key="index" class="text-center">
+                <ul class="list-unstyled d-flex justify-content-center">
+                    <li v-for="pizza,index in menuPizza" :key="index" class="text-center position-relative">
                         <img :src="pizza.src" alt="">
                         <div class="text-tertiary">{{ pizza.nome }}</div>
-                        <div class="text-danger">{{ pizza.costo_massimo }}</div>
+                        <div class="text-danger"><span v-show="pizza.costo_minimo">{{ pizza.costo_minimo }} - </span>{{ pizza.costo_massimo }}</div>
+                        <div class="badge-sold" v-show="pizza.sold">
+                            <span class="text-white fs-10">SOLD</span>
+                        </div>
                     </li>
                 </ul>
+            </div>
+            <div class="square">
+                <img src="/public/svg/svg-10.svg" alt="">
             </div>
         </div>
     </div>
@@ -80,8 +86,50 @@ export default {
         margin: 30px 0px;
     }
 
+    ul {
+        margin-bottom: 50px;
+    }
+
+    ul:hover {
+        overflow-x: auto;
+    }
+
+    li {
+        margin: 0px 30px;
+    }
+
     li img {
         width: 150px;
+        height: auto;
+    }
+
+    .badge-sold {
+        position: absolute;
+        top: 0;
+        right: 10px;
+        background-color: var(--bg-button);
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .square {
+        width: 30px;
+        height: 30px;
+        background-color: var(--bg-tertiary);
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .square img {
+        width: 15px;
         height: auto;
     }
 </style>
